5c4f74bad271f2f0c9a6c3c5c91fdf0efc2dbb78
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-24 10:22:39 -0600

    added aquabats to artists

--- SQLIntro/FirstSql.sql
+++ SQLIntro/FirstSql.sql
@@ -1,9 +1,5 @@
-﻿SELECT AlbumId, ArtistName Artist, Album.Title Album, Song.Title Songs From Album
-Left JOIN Song
-on Song.AlbumId = Album.Id
-Left Join Artist
-on Song.ArtistId = Artist.Id
-Left Join Genre
-on Song.GenreId = Genre.Id
-WHERE Song.Title IS NULL
-ORDER BY ArtistName
+﻿Select YearEstablished from Artist where ArtistName = 'Aquabats'
+
+update Artist
+set YearEstablished = 1994
+Where ArtistName = 'Aquabats'
