cdf05f5684a1521aae278d58f1672dd72c3d2696
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-24 15:18:25 -0600

    now shows the lengthiest record

--- SQLIntro/FirstSql.sql
+++ SQLIntro/FirstSql.sql
@@ -1,6 +1,5 @@
 ﻿
-SELECT DISTINCT [ArtistName] From Artist
-Left JOIN Album
-on Artist.Id = Album.ArtistId
-Group By ArtistName
-Having Count(Album.Label) > 1
+SELECT * from Album
+Where AlbumLength = (SELECT MAX(AlbumLength) from Album)
+
+
